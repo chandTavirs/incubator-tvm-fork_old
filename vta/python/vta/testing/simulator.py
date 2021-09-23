d44d430bb8f0084f0061e4d86aeed25f43352ab2
Suvadeep Banerjee <suvadeep.banerjee@intel.com>
2021-09-23 14:46:52 -0700

    Comment fix

--- vta/python/vta/testing/simulator.py
+++ vta/python/vta/testing/simulator.py
@@ -14,6 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+# Modified by contributors from Intel Labs
+
 """Utilities to start simulator."""
 import ctypes
 import json
