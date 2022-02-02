0b04f057856ed47506392a04e62592e930abb66f
Suvadeep Banerjee <suvadeep.banerjee@intel.com>
2022-02-02 09:33:11 -0800

    Tiling Parameter Search for ALU-based depthwise schedule

--- vta/python/vta/top/vta_depthwise_conv2d.py
+++ vta/python/vta/top/vta_depthwise_conv2d.py
@@ -18,6 +18,10 @@
 # Modified by contributors from Intel Labs
 
 """Depthwise Conv2D operator declaration and schedule registration for VTA."""
+import os
+
+import itertools
+from functools import reduce
 
 import tvm
 from tvm import te
@@ -26,6 +30,163 @@ from tvm import topi
 
 from .utils import is_packed_layout
 from ..environment import get_env
+
+def facc(tilevec, wklparams, env, fallback=False):
+    """
+    Function to compute acc scratchpad usage and accload_nbytes
+
+    Parameters
+    ----------------
+    tilevec : list
+        Tiling parameter candidate
+
+    wklparams : dict
+        Workload parameters
+
+    env : vta.environment.Environment
+        VTA environment parameters
+
+    Returns
+    ---------------
+    accbits : int
+        Utilized acc scratchpad (in bits)
+
+    accload_bytes : int
+        Number of bytes loaded from DRAM -> acc
+    """
+    _, tile_co, tile_h, tile_w = tilevec
+    i_h, i_w = wklparams['i_h'], wklparams['i_w']
+    k_h, k_w = wklparams['k_h'], wklparams['k_w']
+    s_h, s_w = wklparams['s_h'], wklparams['s_w']
+    p_h, p_w = wklparams['p_h'], wklparams['p_w']
+    o_h, o_w = wklparams['o_h'], wklparams['o_w']
+    f_o = wklparams['f_o']
+
+    inpload_height = (((i_h/tile_h + 2*p_h - k_h)//s_h)*s_h + k_h)
+    inpload_width = (((i_w/tile_w + 2*p_w - k_w)//s_w)*s_w + k_w)
+
+    inpuse = (inpload_height * inpload_width * env.BLOCK_OUT)
+
+    wgtuse = (k_h * k_w * env.BLOCK_OUT)
+    accbytes = inpuse + wgtuse
+
+    inpload = (f_o * ((tile_h * inpload_height) - 2) *
+               ((tile_w * inpload_width) -2))
+    wgtload = (k_h * k_w * f_o * tile_h * tile_w)
+    accload_bytes = ((inpload + wgtload) * env.ACC_WIDTH)/8
+
+    if fallback is True:
+        accbits = accbytes * env.ACC_WIDTH
+        return accbits, accload_bytes
+    prodbytes = (k_h * k_w * (f_o/tile_co) *
+                 o_h/tile_h * o_w/tile_w)
+    accbits = (accbytes + prodbytes) * env.ACC_WIDTH
+    return accbits, accload_bytes
+
+def cons(tilevec, wklparams, env):
+    """
+    Function to check constraints during tiling parameter search
+
+    Parameters
+    ------------
+    tilevec : list
+        Tiling parameter candidate
+
+    wklparams : dict
+        Workload parameters
+
+    env : vta.environment.Environment
+        VTA environment parameters
+
+    Returns
+    --------------
+    cost : int
+        Cost of tiling parameters, if constraint unviolated
+        Else, return False
+    """
+    # Support 2-dimensional Uop Hash Map at a maximum
+    bounds = [wklparams['w_b']//env.BATCH, wklparams['f_o']//env.BLOCK_OUT,
+              wklparams['o_h'], wklparams['o_w']]
+    dimcount = 0
+    for i, _ in enumerate(bounds):
+        if tilevec[i] != bounds[i]:
+            dimcount += 1
+    if dimcount > 2:
+        return False
+
+    cost = 0
+    fallback = True
+    for i, _ in enumerate(bounds):
+        if tilevec[0] != bounds[i]:
+            fallback = False
+            break
+    accbits, loadbytes = facc(tilevec, wklparams, env, fallback)
+    accsize = env.ACC_BUFF_SIZE * 8
+    accfit = accsize - accbits
+    if accfit < 0:
+        return False
+    cost += loadbytes
+
+    return int(cost)
+
+def find_factors(n):
+    return list(set(reduce(list.__add__,
+                           ([i, n//i] for i in range(1, int(n**0.5) + 1) if not n % i))))
+
+def tileparam_search(wkl, env):
+    """
+    Function to search for tiling parameters
+
+    Parameters
+    ---------------
+    wkl : tvm.ir.container.Array
+        Workload container from conv2d stage
+
+    env: vta.environment.Environment
+        VTA environment parameters
+
+    Returns
+    --------------
+    soln : list
+        Optimal tiling parameters
+    """
+    # Collect parameters
+    wklparams = {}
+    wklparams['w_b'] = int(wkl[1][1][0]) * env.BATCH
+    wklparams['i_h'] = int(wkl[1][1][2])
+    wklparams['i_w'] = int(wkl[1][1][3])
+    wklparams['k_h'] = int(wkl[2][1][2])
+    wklparams['k_w'] = int(wkl[2][1][3])
+    wklparams['f_o'] = int(wkl[1][1][1] * wkl[1][1][5])
+    wklparams['s_h'] = int(wkl[3][0])
+    wklparams['s_w'] = int(wkl[3][1])
+    wklparams['p_h'] = int(wkl[4][0])
+    wklparams['p_w'] = int(wkl[4][1])
+    wklparams['o_h'] = ((wklparams['i_h'] + 2*wklparams['p_h'] - wklparams['k_h'])//
+                        wklparams['s_h'] + 1)
+    wklparams['o_w'] = ((wklparams['i_w'] + 2*wklparams['p_w'] - wklparams['k_w'])//
+                        wklparams['s_w'] + 1)
+
+    # Create tiling candidates
+    bounds = [wklparams['w_b']//env.BATCH, wklparams['f_o']//env.BLOCK_OUT,
+              wklparams['o_h'], wklparams['o_w']]
+    combinations = list(itertools.product(*[find_factors(bounds[i]) for i in range(len(bounds))]))
+    values = []
+
+    # Search against constraints and collect cost of candidates
+    for combination in combinations:
+        cost = cons(combination, wklparams, env)
+        if cost:
+            values.append((combination, cost))
+    # Use fallback if TPS fails to search valid parameters
+    if not values:
+        return None
+
+    best = [c for c, v in values if v == min([v for c, v in values])]
+    soln = [None] * len(bounds)
+    for i, _ in enumerate(bounds):
+        soln[i] = ([best[0][i], bounds[i]//best[0][i]])
+    return soln
 
 @autotvm.register_topi_compute("depthwise_conv2d_packed.vta")
 def depthwise_conv2d_packed(cfg, data, kernel, strides, padding, dilation, layout, out_dtype):
@@ -163,6 +324,56 @@ def schedule_depthwise_conv2d_packed(cfg, outs):
     cfg.define_split('tile_h', x_i, num_outputs=2)
     cfg.define_split('tile_w', x_j, num_outputs=2)
     cfg.define_split('tile_co', x_co, num_outputs=2)
+
+    # Call TPS to get optimum tiling
+    if cfg.is_fallback and env.ENABLE_TPS:
+        wkl = sum_ops[0].attrs['workload']
+        tiles = tileparam_search(wkl, env)
+        if tiles is not None:
+            tile_b, tile_co, tile_h, tile_w = tiles
+            if env.ENABLE_TILING_WRITEOUT:
+                filepath = os.path.join(os.environ['TVM_HOME'], "ash", "tps.log")
+                config_key = "x".join([str(env.BATCH), str(env.BLOCK_IN), str(env.BLOCK_OUT)])
+                config_key = "_".join([config_key, "i8w8a32"])
+                config_key = "_".join([config_key, str(env.LOG_UOP_BUFF_SIZE)])
+                config_key = "_".join([config_key, str(env.LOG_INP_BUFF_SIZE)])
+                config_key = "_".join([config_key, str(env.LOG_WGT_BUFF_SIZE)])
+                config_key = "_".join([config_key, str(env.LOG_ACC_BUFF_SIZE)])
+                f = open(filepath, "a+")
+                f.write('{"input": ["ext_dev -device=vta -keys=cpu -model=sim_')
+                f.write(config_key)
+                f.write('", "')
+                f.write(str(wkl[0]))
+                f.write('", [')
+                outstr = str(wkl[1])
+                for i in range(2, len(wkl)-2):
+                    outstr = ', '.join([outstr, str(wkl[i])])
+                f.write(outstr)
+                f.write(', "')
+                f.write(str(wkl[-2]))
+                f.write('", "')
+                f.write(str(wkl[-1]))
+                f.write('"], {}]')
+                f.write(', "config": {"index": 1, "code_hash": null, "entity": [')
+                outstr = ', '.join(['["tile_b"', '"sp"', str(tile_b)])
+                outstr = ''.join([outstr, ']'])
+                outstr = ', '.join([outstr, '["tile_h"', '"sp"', str(tile_h)])
+                outstr = ''.join([outstr, ']'])
+                outstr = ', '.join([outstr, '["tile_w"', '"sp"', str(tile_w)])
+                outstr = ''.join([outstr, ']'])
+                outstr = ', '.join([outstr, '["tile_co"', '"sp"', str(tile_co)])
+                outstr = ''.join([outstr, ']]}'])
+                f.write(outstr)
+                f.write(', "result": [1, 0, 1, 1], "version": 0.2')
+                f.write(', "tvm_version": "0.7.dev0"}')
+                f.write('\n')
+                f.close()
+            cfg['tile_b'] = tvm.autotvm.task.space.SplitEntity(tile_b)
+            cfg['tile_h'] = tvm.autotvm.task.space.SplitEntity(tile_h)
+            cfg['tile_w'] = tvm.autotvm.task.space.SplitEntity(tile_w)
+            cfg['tile_co'] = tvm.autotvm.task.space.SplitEntity(tile_co)
+            print("Replacing fallback parameters with optimal tiling parameters" \
+                " for DRAM load minimization")
 
     # Apply tiling
     x_bo0, x_bo1 = cfg['tile_b'].apply(s, output, x_bo)
